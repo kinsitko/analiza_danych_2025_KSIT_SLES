4cb451c1ddbf9c56b55bafbe62e06b4d7ad8fccb
kinsitko <kinsitko@gmail.com>
2025-09-21 16:46:43 +0200

    edit utils, imports, skopiowanie config_data do glownego

---
+++ final_grade_model/src/utils/utils.py
@@ -0,0 +1,29 @@
+# --- Data utils ---
+from kaggle.api.kaggle_api_extended import KaggleApi
+import pandas as pd
+import zipfile, os
+
+def load_kaggle_dataset(dataset_name: str, zip_file_name: str, csv_file_name: str, data_path: str = 'data') -> pd.DataFrame:
+    api = KaggleApi()
+    api.authenticate()
+    os.makedirs(data_path, exist_ok=True)
+    # api.dataset_download_files(dataset_name, path=data_path, unzip=False)
+    zip_path = os.path.join(data_path, zip_file_name)
+
+    if not os.path.exists(zip_path):
+        print(f"Pobieram dataset {dataset_name}...")
+        api.dataset_download_files(dataset_name, path=data_path, unzip=False)
+    else:
+        print(f"Plik {zip_file_name} już istnieje – pomijam pobieranie.")
+
+    with zipfile.ZipFile(zip_path, "r") as z:
+        print("Pliki w ZIP:", z.namelist())
+
+    with zipfile.ZipFile(zip_path, 'r') as z:
+        with z.open(csv_file_name) as f:
+            df = pd.read_csv(f)
+
+    return df
+
+# --- Plot utils ---
+# none
